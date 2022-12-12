6d259c3f057c75e3dab0f33c1451a7a9b6030991
sanam1060 <sanam_g1990@yahoo.com>
2022-12-12 11:25:15 -0500

    modify Nav components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import "./App.css";
 import Nav from "./Components/Nav";
+import { useState } from "react";
 
 function App() {
+    const [navLinks] = useState([
+      {
+        name: "About Me",
+      },
+      {
+        name: "Portfolio",
+      },
+      {
+        name: "Contact",
+      },
+    ]);
+    const [currentNavLink, setCurrentNavLink] = useState(navLinks[0]);
   return (
     <div>
-      <Nav />
+      <Nav navLinks={navLinks} currentNavLink={currentNavLink} setCurrentNavLink={setCurrentNavLink}></Nav>
     </div>
   );
 }
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,5 +1,42 @@
 function Nav() {
-  return <div>Navigation</div>;
+  return
+    <nav className="navbar navbar-expand-md bg-light border border-secondary border-opacity-25">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="/">
+          Sanam Ganjian
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            {props.navLinks.map((navLink) => (
+              <li className="nav-item mx-2" key={navLink.name}>
+                <span
+                  className={`nav-link ${
+                    navLink.name === props.currentNavLink.name && "active"
+                  }`}
+                  onClick={() => props.setCurrentNavLink(navLink)}
+                >
+                  {navLink.name}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>;
 }
 
 export default Nav;
